a30b0ef4f3e1f9de2df0137d1c5dd232d9fc8a09
Serhii Vidernikov <sv54@mscloud.ua.es>
2021-12-22 12:28:37 +0100

    todavia probando

--- main.py
+++ main.py
@@ -3,44 +3,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 import utils
 import adaboost
-import os
 import clasificador_debil as cd
 
 
 #Entrenar los 10 clasificadores fuertes
 def entrenarCFs(X,mnist_Y):
     listaCFs=[0,1,2,3,4,5,6,7,8,9]
-    num=1000 #numero de imagenes
-    incr=100 
+    num=100 #numero de imagenes
+    #incr=100 
     A=50 #Num de intentos para cada clasificador
-    T=100 #Num de clasificadores debiles
+    T=10 #Num de clasificadores debiles
     for i in range(10):
-        Y=utils.adaptarY(mnist_Y,i)
+        Y=utils.adaptarY(mnist_Y,i) #adaptamos el conjunto a la clase 
         listaCFs[i]=adaboost.entrenar(X[0:num],Y[0:num],T,A)
-
+    #print(listaCFs)
     return listaCFs
+
+def entrenarCF(X,Y,clase):
+    num=5000
+    T=100
+    A=100
+    Y=utils.adaptarY(Y,clase)
+    CFentrenado=adaboost.entrenar(X[0:num],Y[0:num],T,A)
+    return CFentrenado
+
+def testCF(X,Y,CF,clase):
+    num=10000
+    incr=2000
+    for j in range(10):
+        
+        Xi=X[num:num+incr]
+        Yi=Y[num:num+incr]
+        num=num+incr
+        Yi=utils.adaptarY(Yi,clase)
+        resul=adaboost.test(Xi,Yi,CF)
+        contador=0
+        y1=0
+        for i in range(len(resul)):
+            if(resul[i]==Yi[i] and resul[i]==1):
+                contador=contador+1
+        for i in range(len(Yi)):
+            if Yi[i]==1:
+                y1=y1+1
+        print ((contador/y1)*100)
+    return 0
 
 
 def testCFs(X,Y,listaCFs):
-    print(len(listaCFs))
+    #print(len(X))
     listaImgRes=np.full((len(X),10),[0]) #[[-1,-1,1,-1,-1,-1,-1,-1,-1,-1][-1,-1,1,-1,-1,-1,-1,-1,-1,-1]]
     for i in range(len(listaCFs)):
         ResClase=adaboost.test(X,Y,listaCFs[i])
-        for j in range(10):
+        for j in range(len(X)):
             listaImgRes[j][i]=ResClase[j]
 
-    
+
     resultado=0
     for i in range(len(X)):
         ultimo=-1
         for j in range(10):
-            if(listaImgRes[i][j]==1):
-                ultimo=j
-                if (Y[i]==ultimo):
-                    resultado=resultado+1
+            if(listaImgRes[i][j]==1 and listaImgRes[i][j]==Y[i]):
+                resultado+=1
+                #print("imagen "+str(i)+" pertenece a la clase "+str(j))
+                break
+                # ultimo=j
+                # if (Y[i]==ultimo):
+                #     resultado=resultado+1    
                 
-    
-
+    for i in listaImgRes:
+       print(i)
     print(resultado/len(X)*100)
 
     return 0
@@ -55,12 +86,18 @@ def main():
     mnist_Y = npzfile['y']
     #utils.mostrar_imagen(mnist_X[0])
     #print(mnist_Y[0])
+    #utils.mostrar_imagen(mnist_X[30001])
     X,Y=utils.adaptar_conjuntos(mnist_X,mnist_Y)
+    #CF=entrenarCF(X,mnist_Y,0)
+    #testCF(X,mnist_Y,CF,0)
+
 
 
     listaCF=entrenarCFs(X,mnist_Y)
-    X=X[30000:31000]
-    Y=Y[30000:31000]
+    #print(listaCF)
+    
+    X=X[30000:32000]
+    Y=Y[30000:32000]
     testCFs(X,Y,listaCF)
 
 
@@ -99,15 +136,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-
-
-# Mostrar una imagen y su etiqueta
-
-
-# Adaptar los conjuntos X e Y a AdaBoost
 
 
 
